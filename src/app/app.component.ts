2ab8128697cddb09b732f45300b3f1635ed56ebf
oleksandr dombrovskyi <dombrovskyi.oleksandr@gmail.com>
2017-04-10 21:33:02 +0300

    Refactoring: all files were refactored with a unique code style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from'@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
+import {Component} from'@angular/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import {OnInit} from '@angular/core';
 
 @Component({
     providers: [HeroService],
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
@@ -17,18 +17,20 @@ import { OnInit } from '@angular/core';
             </li>
         </ul>
         <hero-detail [hero]="selectedHero"></hero-detail>
-        `,
+    `,
     styles: [`
         .selected {
             background-color: #CFD8DC !important;
             color: white;
         }
+
         .heroes {
             margin: 0 0 2em 0;
             list-style-type: none;
             padding: 0;
             width: 15em;
         }
+
         .heroes li {
             cursor: pointer;
             position: relative;
@@ -39,19 +41,23 @@ import { OnInit } from '@angular/core';
             height: 1.6em;
             border-radius: 4px;
         }
+
         .heroes li.selected:hover {
             background-color: #BBD8DC !important;
             color: white;
         }
+
         .heroes li:hover {
             color: #607D8B;
             background-color: #DDD;
             left: .1em;
         }
+
         .heroes .text {
             position: relative;
             top: -3px;
         }
+
         .heroes .badge {
             display: inline-block;
             font-size: small;
@@ -71,7 +77,8 @@ import { OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
 
-    constructor(private heroService: HeroService) { }
+    constructor(private heroService: HeroService) {
+    }
 
     ngOnInit(): void {
         this.getHeroes();
@@ -85,14 +92,15 @@ export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     };
 
-	hero: Hero = {
-		id: 1,
-		name: 'Windstrom'
-	};
+    hero: Hero = {
+        id: 1,
+        name: 'Windstrom'
+    };
 }
 
 
