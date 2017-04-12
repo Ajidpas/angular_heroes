ae8da21733595db1a015773221a352da9657ac70
oleksandr dombrovskyi <dombrovskyi.oleksandr@gmail.com>
2017-04-12 21:46:26 +0300

    Router: global refactoring and styling

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -8,10 +8,11 @@ import {HeroService} from './hero.service';
 
 @Component({
     selector: 'hero-detail',
-    templateUrl: './hero-detail.component.html'
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-    @Input() hero: Hero;
+    hero: Hero;
 
     constructor (private heroService: HeroService, private route: ActivatedRoute, private location: Location) {}
 
